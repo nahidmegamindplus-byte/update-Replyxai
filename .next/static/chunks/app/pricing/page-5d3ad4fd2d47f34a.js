f6d3373be6e49a194328e7e90f13868a18f22edb
Naimul Islam <naimulislam@Naimuls-Mac-mini.local>
2026-09-07 02:10:26 +0600

    feat: commit production .next & _next build artifacts for zero-terminal Hostinger deployment

---
+++ .next/static/chunks/app/pricing/page-5d3ad4fd2d47f34a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9979],{2636:function(n,e,u){Promise.resolve().then(u.bind(u,5169))}},function(n){n.O(0,[2972,5169,2971,2117,1744],function(){return n(n.s=2636)}),_N_E=n.O()}]);
